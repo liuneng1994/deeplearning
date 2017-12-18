a98879cb17828b35f76bc8292c0617492b3a53c8
liuneng <1398775315@qq.com>
2017-12-18 16:19:59 +0800

    新增性能测试代码

---
+++ dataset_test/numpy_test.py
@@ -0,0 +1,11 @@
+from cifa100.cifar100 import load_data
+import numpy as np
+import datetime
+
+(train_data, train_label), (test_data, test_label) = load_data()
+batches = np.split(train_data, 20, 0)
+start = datetime.datetime.now()
+for batch in batches:
+    batch.shape
+end = datetime.datetime.now()
+print("cost %d ms" % (end - start).microseconds)
